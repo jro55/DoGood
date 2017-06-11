fb65d5149a8b093da49b3603e0feeb2feb6716b3
jRo <josh.roman@motili.com>
2017-06-11 17:31:27 -0600

    got navigator set up fool

--- App.js
+++ App.js
@@ -4,16 +4,17 @@
 
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Router from './App/Components/Router/Router';
 
 export default class App extends React.Component {
   render() {
-    // return <Router />;
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Yo</Text>
-      </View>
-    );
+    return <Router />;
+    // return (
+    //   <View style={styles.container}>
+    //     <Text>Open up App.js to start working on your app!</Text>
+    //     <Text>Yo</Text>
+    //   </View>
+    // );
   }
 }
 
