2b53cb4f9e18b69c47b68d47e96302c6026ec82a
jRo <josh.roman@motili.com>
2017-06-11 16:14:30 -0600

    commit

---
+++ App.js
@@ -0,0 +1,27 @@
+/**
+ * Copyright JRoCoCo, 2017 All Rights Reserved
+ */
+
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    // return <Router />;
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Yo</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
